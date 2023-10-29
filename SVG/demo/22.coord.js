8b12d9129705fa0583eef23cc66898c3c4fd4f63
muddyrain <qiu3291002845@gmail.com>
2023-10-29 21:34:00 +0800

    feat: 22.动态折线图xy轴绘制

---
+++ SVG/demo/22.coord.js
@@ -0,0 +1,59 @@
+/**
+ *
+ * @param {string |
+ *  {
+ *  id:string;
+ *  color:string;
+ *  xAxis:boolean;
+ *  yAxis:boolean;
+ *  title:string;
+ *  }
+ * } arg
+ * @returns {void}
+ */
+function draw(arg) {
+  if (typeof arg === 'string') {
+    arg = { id: string };
+  }
+  const config = {
+    color: '#ccc',
+    title: '标题',
+    titleColor: '#000',
+    xAxis: true,
+    yAxis: true,
+    count: 5,
+    ...arg,
+  };
+  const svg = document.querySelector(config.id);
+  const NS = 'http://www.w3.org/2000/svg';
+
+  svg.setAttribute('viewBox', '0 0 250 250');
+  const path = document.createElementNS(NS, 'path');
+  const g = document.createElementNS(NS, 'g');
+  const title = document.createElementNS(NS, 'text');
+  title.innerHTML = config.title;
+  title.setAttribute('x', '10');
+  title.setAttribute('y', '15');
+  title.setAttribute('fill', `${config.titleColor}`);
+  title.setAttribute('font-size', '8');
+  g.appendChild(title);
+  path.setAttribute('stroke', `${config.color}`);
+  path.setAttribute('stroke-width', '0.5');
+  let d = '';
+  const space = 200 / config.count;
+  for (let i = 0; i <= config.count; i++) {
+    if (config.xAxis) {
+      d += `M25 ${25 + i * space} H225 `;
+    }
+    if (config.yAxis) {
+      d += `M${25 + i * space} 25 V225 `;
+    }
+  }
+  path.setAttribute('d', d);
+  g.appendChild(path);
+  if (svg.children.length > 0) {
+    svg.insertBefore(g, svg.children[0]);
+  } else {
+    svg.appendChild(g);
+  }
+}
