57bdcd596ec4bbdb9849c56aa6553567c95d818e
“muddyrain” <qiu3291002845@gmail.com>
2024-01-11 11:41:51 +0800

    feat: npm 发布测试

---
+++ rollup-npm/rollup.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'rollup';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
+
+export default defineConfig({
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'esm',
+    name: 'rollup-npm',
+  },
+  external: ['lodash-es'],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+  ],
+});
