01e70d3838f0ccd9c5895c1767354e468ce140a8
“muddyrain” <qiu3291002845@gmail.com>
2024-01-11 10:29:28 +0800

    feat: react-rollup-compile finish

--- rollup-react/rollup.config.ts
+++ rollup-react/rollup.config.ts
@@ -5,6 +5,15 @@ import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
 import htmlTemplate from 'rollup-plugin-generate-html-template';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import clear from 'rollup-plugin-clear';
+import postcss from 'rollup-plugin-postcss';
+import image from '@rollup/plugin-image';
+import alias from '@rollup/plugin-alias';
+import { fileURLToPath } from 'node:url';
+import { terser } from 'rollup-plugin-terser';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 const config: RollupOptions = {
   input: 'src/main.tsx',
@@ -13,7 +22,23 @@ const config: RollupOptions = {
     format: 'esm',
     name: 'rollup-react-ts',
     sourcemap: true,
+    entryFileNames: 'chunk-[name]-[hash].js',
+    manualChunks: {
+      react: ['react', 'react-dom'],
+    },
+    plugins: [terser()],
+    // cdn 引入
+    // globals: {
+    //   react: 'React',
+    //   'react-dom': 'ReactDOM',
+    // },
+    // paths: {
+    //   react: 'https://cdn.jsdelivr.net/npm/react@18.2.0/+esm',
+    //   'react-dom': 'https://cdn.jsdelivr.net/npm/react-dom@18.2.0/+esm',
+    // },
   },
+  // 剔除打包文件中的 react 和 react-dom
+  // external: ['react', 'react-dom'],
   plugins: [
     nodeResolve(),
     commonjs(),
@@ -22,17 +47,47 @@ const config: RollupOptions = {
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
+    clear({
+      targets: ['dist'],
+      watch: true,
+    }),
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: fileURLToPath(new URL('src', import.meta.url)),
+        },
+      ],
+    }),
     babel({
       babelHelpers: 'runtime',
       exclude: 'node_modules/**',
       include: ['src/**/*'],
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
     }),
+    serve({
+      open: false,
+      contentBase: 'dist',
+      host: 'localhost',
+      port: 3000,
+    }),
+    livereload('src'),
+    postcss({
+      extensions: ['.css', '.less', '.scss', '.sass'],
+      // 将 css 插入到 style 中
+      extract: true,
+      // 压缩 css
+      minimize: true,
+      // css modules
+      modules: true,
+    }),
     htmlTemplate({
       template: 'public/index.html',
       target: 'dist/index.html',
       attrs: ['type="module"'],
     }),
+    visualizer(),
+    image(),
   ],
 };
 
--- rollup-react/src/App.tsx
+++ rollup-react/src/App.tsx
@@ -1,14 +1,42 @@
-import React, { FC } from 'react';
-
+import React, { FC, Suspense, lazy } from 'react';
+import styles from '@/styles/app.module.scss';
+import image1 from '@/assets/1.jpeg';
+import Loading from './components/loading';
+const About = lazy(() => delayDemo(import('@/components/About'), 2000));
 const App: FC = () => {
   const [count, setCount] = React.useState(0);
+  const [visible, setVisible] = React.useState(false);
   return (
     <div>
       <h2>hello world</h2>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <img width={200} height={200} src={image1} alt='' />
+      <button className={styles['button']} onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <hr />
+      <button
+        className={styles['button']}
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        show about
+      </button>
+      {visible && (
+        <Suspense fallback={<Loading />}>
+          <About />
+        </Suspense>
+      )}
     </div>
   );
 };
+
+//添加一个固定延迟，以便看到加载效果
+async function delayDemo(promise: Promise<any>, delay: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay);
+  }).then(() => promise);
+}
 
 export default App;
